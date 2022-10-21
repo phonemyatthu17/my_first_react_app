f7b7ad02d7135dd7b9c69240263065f42e300228
unknown <phonemashelby@gmail.com>
2022-10-21 10:16:38 +0630

    css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect } from "react";
 
-function App() {
-  const [counter, setCounter] = useState(1);
+const API_URL = "http://www.omdbapi.com?apikey=ce5efd2c";
 
-  return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
-        -
-      </button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((nextCount) => nextCount + 1)}>
-        +
-      </button>
-    </div>
-  );
-}
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("the godfather");
+  }, []);
+
+  return <h1>App</h1>;
+};
 
 export default App;
