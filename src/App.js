bf65ac1cf4a4d31f415fcafe111f5a2973e350a7
unknown <phonemashelby@gmail.com>
2022-10-23 19:26:41 +0630

    css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
 
+import "./App.css";
+import SearchIcon from "./search.svg";
 const API_URL = "http://www.omdbapi.com?apikey=ce5efd2c";
 
 const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-
     console.log(data.Search);
   };
 
@@ -14,7 +15,11 @@ const App = () => {
     searchMovies("the godfather");
   }, []);
 
-  return <h1>App</h1>;
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+    </div>
+  );
 };
 
 export default App;
