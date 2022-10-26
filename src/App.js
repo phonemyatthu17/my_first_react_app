44722ea98deef3d43bfeb82bf361e76ec6a1cba7
unknown <phonemashelby@gmail.com>
2022-10-26 18:15:18 +0630

    jsx js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import { useEffect } from "react";
-
+import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 const API_URL = "http://www.omdbapi.com?apikey=ce5efd2c";
+
+const movie = {
+  Title: "The Godfather",
+  Year: "1972",
+  imdbID: "tt0068646",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+};
 
 const App = () => {
   const searchMovies = async (title) => {
@@ -18,6 +27,19 @@ const App = () => {
   return (
     <div className="app">
       <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        ></input>
+        <img src={SearchIcon} alt="search" onClick={() => {}}></img>
+      </div>
+
+      <div className="container">
+        <MovieCard movie={movie} />
+      </div>
     </div>
   );
 };
